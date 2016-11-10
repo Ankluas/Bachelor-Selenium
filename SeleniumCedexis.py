34d401ae293d01e7e0d7ca8aaf8680b61e1cf1fa
Ankluas <w.submail94@gmail.com>
2016-11-10 14:13:10 +0100

    Update SeleniumCedexis.py

--- SeleniumCedexis.py
+++ SeleniumCedexis.py
@@ -3,19 +3,27 @@ from selenium.webdriver.common.keys import Keys
 import time
 
 driver = webdriver.Chrome('E:/Programme/chromedriver.exe')
+driver.set_page_load_timeout(20)
 counter = 0
 list = open("E:/Eigene Dateien/list/list.txt", "r")
 
-for line in list:
-    if line.strip():
-        driver.get(line)
-        time.sleep(3)
-        html = driver.execute_script("return document.documentElement.outerHTML")
-        if (html.find("cedexis") == -1):
-            print("not found")
-        else:
-            print("found")
-            counter += 1
+while True:
+    try:
+        for line in list:
+            if line.strip():
+                driver.get(line)
+                time.sleep(3)
+                html = driver.execute_script("return document.documentElement.outerHTML")
+                if (html.find("cedexis") == -1):
+                    print("not found")
+                else:
+                    print("found")
+                    counter += 1
+    except:
+        TimeoutError
+        print("Site couldn't load!")
+        continue
+    break
 
 
 list.close()
